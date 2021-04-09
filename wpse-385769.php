571f1dfe02f92c0f432caab862822a1dd8299e74
Sally <comp.misc@gmail.com>
2021-04-09 23:09:03 +0800

    changed the plugin page slug to wpse-385769; removed the inline JS and enqueued create-contact.js; changed the post type args

--- wpse-385769.php
+++ wpse-385769.php
@@ -5,8 +5,10 @@
  * Description: Just testing a custom REST controller class for <a href="/wp-json/wp/v2/contact">/wp-json/wp/v2/contact</a> (creating a Contact CPT post <strong>without authentication</strong>). :)
  * Author: Sally CJ
  * Author URI: https://wordpress.stackexchange.com/users/137402/sally-cj
- * Version: 20210408.1
+ * Version: 20210409.1
  */
+
+define( 'WPSE_385769_VERSION', '20210409.1' );
 
 add_action( 'init', 'wpse_385769_init' );
 function wpse_385769_init() {
@@ -17,10 +19,20 @@ function wpse_385769_init() {
 	register_post_type( 'contact', array(
 		'supports'              => array( 'title' ),
 		'has_archive'           => false,
+
+		// Make this CPT available in the REST API.
 		'show_in_rest'          => true,
+
 		// Make sure the class name is correct.
 		'rest_controller_class' => 'My_WP_REST_Contact_Controller',
-		'public'                => true,
+
+		// Contact data are private, so you might want to make this CPT a
+		// private one:
+		'public'                => false,
+		'publicly_queryable'    => true,
+		'show_ui'               => true,
+		'exclude_from_search'   => true,
+
 		'labels'                => array(
 			'name'         => 'Contact DB',
 			'add_new_item' => 'Add New Contact',
@@ -36,57 +48,37 @@ function wpse_385769_init() {
 add_action( 'admin_menu', 'wpse_385769_admin_menu' );
 function wpse_385769_admin_menu() {
 	$title = 'Contact CPT API Endpoint Tester';
-	$hook = add_management_page( $title, $title, 'manage_options',
-		sanitize_title( $title ), 'wpse_385769_admin_page' );
+
+	add_management_page( $title, $title, 'manage_options',
+		'wpse-385769', 'wpse_385769_admin_page' );
 }
 
 function wpse_385769_admin_page() {
 	$title = 'Contact CPT API Endpoint Tester';
-	$api_url = rest_url( 'wp/v2/contact' );
 	?>
 		<div class="wrap">
 			<h1><?php echo esc_html( $title ); ?></h1>
-			<p style="max-width: 50%">
-				Click the button to create a Contact CPT <strong>without authentication</strong>:
-				(the JS uses ESNext or modern JS, so be sure your browser is the latest one...)
-				<button type="button" class="button" onclick="createContact( this )">Create Post</button>
+
+			<p>
+				Click the button to create a Contact post <strong>without authentication</strong>:<br>
+				(the JS script uses ESNext or modern JS, so be sure your browser is the latest one...)
 			</p>
+			<p>
+				<button type="button" class="button" onclick="createContact( this, 'draft' )">Create Contact (status = draft)</button> or
+				<button type="button" class="button" onclick="createContact( this, 'publish' )">Create Contact (status = publish)</button>
+			</p>
+
 			<div id="ajax-res"></div>
 		</div>
-
-		<!-- Just for testing, so I put this script here. -->
-		<script>
-			function createContact( btn ) {
-				const _btnText = btn.text;
-
-				btn.disabled = true;
-				btn.text = 'Loading...';
-
-				fetch( '<?php echo esc_url( $api_url ); ?>', {
-					method: 'POST',
-					body: JSON.stringify( {
-						title: `test via JS fetch() at ${ ( new Date() ).toLocaleString() } :)`,
-					} ),
-					headers: { 'Content-Type': 'application/json' },
-				} )
-					.then( res => res.json() )
-					.then( post => {
-						const res = document.querySelector( '#ajax-res' );
-
-						if ( post.id ) {
-							const link = `<a href="${ post.link }" target="_blank">${ post.id }</a>`;
-							res.innerHTML = `Success! ID: ${ link }; Title: <i>${ post.title.rendered }</i>`;
-						} else if ( post.code ) {
-							res.innerHTML = `Failed! :( Message: <i>${ post.message } (${ post.code })</i>`;
-						} else {
-							res.innerHTML = 'Unknown error - check the console log. Try again later.';
-						}
-						console.log( post );
-
-						btn.text = _btnText;
-						btn.disabled = false;
-					} );
-			}
-		</script>
 	<?php
+}
+
+add_action( 'admin_enqueue_scripts', 'wpse_385769_admin_enqueue_scripts' );
+function wpse_385769_admin_enqueue_scripts( $hook_suffix ) {
+	if ( get_plugin_page_hook( 'wpse-385769', 'tools.php' ) === $hook_suffix ) {
+		wp_enqueue_script( 'admin-create-contact', plugins_url( 'create-contact.js', __FILE__ ), array(), WPSE_385769_VERSION, true );
+		wp_localize_script( 'admin-create-contact', 'wpse_385769', array(
+			'apiRoot' => esc_url_raw( get_rest_url() ),
+		) );
+	}
 }
